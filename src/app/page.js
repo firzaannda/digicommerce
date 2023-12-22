333bef3a51dc6edf6e9012910627d273e6cad42d
Firza Ananda <138755278+firzaannda@users.noreply.github.com>
2023-12-22 14:32:29 +0700

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import { ButtonClick } from "@/components/button/ButtonClick";
+import { Button } from "@nextui-org/react";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Halo firza</h1>
+      <Button color="success">Test</Button>
+      <ButtonClick />
+    </div>
+  );
+}
